15b1610c1c7be510ba93a27de5f3190a57d54a13
Drainel <adamjlicari@gmail.com>
2024-01-14 19:19:13 -0500

    Making it work

--- myApp.js
+++ myApp.js
@@ -2,11 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
 
-app.listen(PORT, () => {
-  console.log(`FreeCodeCamp Project started on port ${PORT}`);
-});
 
 
 
@@ -60,5 +56,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`FreeCodeCamp Project started on port ${port}`);
 });
