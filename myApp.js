cf919da288ce20ebd25462fc5d9e09c4d732c811
Drainel <adamjlicari@gmail.com>
2024-01-14 19:42:06 -0500

    Hide Potentially Dangerous Information Using helmet.hidePoweredBy() V2

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-helmet.hidePoweredBy("X-Powered-By: Express");
+app.use(helmet.hidePoweredBy());
 
 
 
